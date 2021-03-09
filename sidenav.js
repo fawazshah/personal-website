8958df43d18742b8b2338c5bf15629a0622503e4
Fawaz Shah <fawaz010@gmail.com>
2021-03-09 16:54:28 +0000

    Remove sidenav code duplication

---
+++ sidenav.js
@@ -0,0 +1,18 @@
+document.write(`
+  <div class="sidenav">
+    <div class="sidenav-contents">
+      <h1 class="title">FAWAZ</h1>
+      <h1 class="title">SHAH</h1>
+      <h5 class="subheading">4th year<br>MEng Mathematics and Computer Science<br>Imperial College London</h5>
+      <div class="navlinks">
+        <a class="navlink active" href="index.html">About</a>
+        <a class="navlink" href="notes.html">Imperial College Notes</a>
+      </div>
+      <div class="social-media-icons">
+        <a href="https://github.com/fawazshah" class="fab fa-github"></a>
+        <a href="https://www.linkedin.com/in/fawaz-shah" class="fab fa-linkedin-in"></a>
+      </div>
+    </div>
+  </div>
+`)
+
